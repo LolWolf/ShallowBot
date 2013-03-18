c8758432bb525839769c7f1ab676e7b6dce88f1f
Guillermo Angeris <guille@angeris.net>
2013-03-18 09:03:49 -0500

    Implemented all but knights and Pawns.... still missing a check for, well, the check.

--- src/shallowbot/PossibleMoves.java
+++ src/shallowbot/PossibleMoves.java
@@ -13,30 +13,77 @@ public class PossibleMoves {
 		ArrayList<Move> moves = new ArrayList<Move>();
 
 
+
 		return moves;
 	}
 
-	private static ArrayList<Move> generateRookMoves(Board b, int player, ArrayList<Move> moveList) {
-		return null;
+	private static boolean generateRookMoves(Board b, ArrayList<Move> moveList, int position) {
+        boolean _case = false;
+
+        _case |= generateMacroMoves(b, moveList, position, 8, 1);
+        _case |= generateMacroMoves(b, moveList, position, 8, -1);
+        _case |= generateMacroMoves(b, moveList, position, 8, 8);
+        _case |= generateMacroMoves(b, moveList, position, 8, -8);
+
+        return _case;
 	}
 
-	private static boolean generateMacroMoves(Board b, ArrayList<Move> moveList, int pos, int maxTo, int d) {
-		return false;
-	}
+    private static boolean generateKingMoves(Board b, ArrayList<Move> moveList, int position) {
+        boolean _case = false;
 
-	private static boolean inBounds(int position) {
-		return position < 64 && position > 0;
+        _case |= generateMacroMoves(b, moveList, position, 1, 1);
+        _case |= generateMacroMoves(b, moveList, position, 1, -1);
+        _case |= generateMacroMoves(b, moveList, position, 1, 8);
+        _case |= generateMacroMoves(b, moveList, position, 1, -8);
+
+        return _case;
+    }
+
+    private static boolean generateBishopMoves(Board b, ArrayList<Move> moveList, int position) {
+        boolean _case = false;
+
+        _case |= generateMacroMoves(b, moveList, position, 8, 9);
+        _case |= generateMacroMoves(b, moveList, position, 8, 7);
+        _case |= generateMacroMoves(b, moveList, position, 8, -7);
+        _case |= generateMacroMoves(b, moveList, position, 8, -9);
+
+        return _case;
+    }
+
+    private static boolean generateQueenMoves(Board b, ArrayList<Move> moveList, int position) {
+        boolean _case = false;
+
+        _case |= generateBishopMoves(b, moveList, position);
+        _case |= generateRookMoves(b, moveList, position);
+
+        return _case;
+    }
+
+	private static boolean generateMacroMoves(Board b, ArrayList<Move> moveList, int pos, int maxTo, int vec) {
+		int pos_init = pos;
+        while(maxTo>0) {
+            pos += vec;
+
+            //Check bounds
+            if(pos_init%8 < pos%8 || pos > 63 || pos < 0) {
+                return false;
+            }
+
+            //Check for empty/taken pieces.
+            if(b.board[pos]==0) {
+                moveList.add(new Move(pos_init, pos));
+            } else if((b.board[pos] & 8) != 8) {
+                moveList.add(new Move(pos_init, pos));
+                if(b.board[pos]==PieceLabels.W_KING) {
+                    return true;
+                }
+            } else {
+                break;
+            }
+
+            maxTo--;
+        }
+
+        return false;
 	}
-	
-	//Checks for end validity, does not check path taken.
-	/*
-	 HORRID CODE; DO NOT TRY THIS AT HOME, KIDS.
-	 Checks inbounds, checks for looping along the sides of the board.
-	*/
-	private static boolean isValid(int position, int vec, Board b) {
-		return inBounds(position+vec) &&
-				(vec%8==0 ? true : 
-						(position%8+vec<8 && position%8+vec>0));
-	}
-	//End crappy coding.
 }
